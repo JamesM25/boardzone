877bf38568baca11cb219759e1a6bafd653e63a0
James Motherwell <motherwell.james@student.greenriver.edu>
2024-01-15 20:32:13 -0800

    Address linter warnings, add /home.html route

--- src/main/java/edu/greenriver/sdev/boardgames/BoardgamesApplication.java
+++ src/main/java/edu/greenriver/sdev/boardgames/BoardgamesApplication.java
@@ -3,9 +3,18 @@ package edu.greenriver.sdev.boardgames;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A Spring application for the Boardzone website
+ * @author James Motherwell
+ * @version 1.0
+ */
 @SpringBootApplication
 public class BoardgamesApplication {
 
+    /**
+     * Runs the Spring application
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(BoardgamesApplication.class, args);
     }
--- src/main/java/edu/greenriver/sdev/boardgames/controllers/StaticPages.java
+++ src/main/java/edu/greenriver/sdev/boardgames/controllers/StaticPages.java
@@ -3,9 +3,27 @@ package edu.greenriver.sdev.boardgames.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Implements routes for all static pages on the website
+ *
+ * @author James Motherwell
+ * @version 1.0
+ */
 @Controller
 public class StaticPages {
+    /**
+     * @return Logical name of the home page (index)
+     */
     @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    /**
+     * Implement a home.html route to fulfill the requirements on the assignment rubric
+     * @return Logical name of the home page (index)
+     */
+    @GetMapping("/home.html")
     public String home() {
         return "index";
     }
